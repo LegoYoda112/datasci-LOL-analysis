0fc64e7ebd4889ed36b78156446328c320bac4ea
Fananaban <robert.field19@imperial.ac.uk>
2020-11-27 22:37:53 +0000

    Added a logistic regression model!
    It works!

--- leaguedata.py
+++ leaguedata.py
@@ -27,6 +27,9 @@ class match:
     def bResult(self):
         return (self.info()['bResult'] == 1).bool()
     
+    def length(self):
+        return int(self.info()['gamelength'].values)
+    
     # ===== BANS =====
     def bans(self):
         return bans.loc[bans['Address'] == self.address]
@@ -54,25 +57,74 @@ class match:
         self_kills = self.kills()
         return self_kills.loc[self_kills['Team'] == 'rKills']
     
-    def last_kill(self,minute):
-        #Returns the time since last kill, the kill object, and its index.
-        prev_kills = {}
-        self_kills = self.kills()
-        for index,kill in enumerate(self_kills.values):
+    
+    def prev_kills(self,minute,team_type = "all"):
+        '''Returns a dictionary object with all kills before a specified minute, containing the time of kill, and its index.
+        Also takes a team type argument for specifying, blue, red, or all.'''
+        
+        prev_kills = pd.DataFrame({'Time' : [],'Index':[],'Blue':[],'XY':[]})
+        
+        if team_type == "red":
+            kill_list = self.red_kills()
+        elif team_type == "blue":
+            kill_list = self.blue_kills()
+        else:
+            kill_list = self.kills()
+        
+        for index,kill in enumerate(kill_list.values):
             a = (minute - kill[2])
             if a > 0:
-                prev_kills[str(a)] = index
-        return min(prev_kills),self_kills.values[prev_kills[min(prev_kills)]],prev_kills[min(prev_kills)]
+                this_kill = pd.DataFrame({'Time' : [kill[2]],
+                                          'Index':[index],
+                                          'Blue':[kill[1] == "bKills"],
+                                          'XY':[self.get_xy(index)]})
+                
+                prev_kills = pd.concat([prev_kills,this_kill], ignore_index=True)
+                
+        return prev_kills.sort_values('Time').reset_index(drop=True)
     
+    def kill_freq(self,minute,time_range = 5,team_type = "all"):
+        '''Gets the kill frequency from a given minute, over a given range (default = 5 mins),
+        for either blue, red, or all teams. (default = all)'''
+        prev_kills = self.prev_kills(minute,team_type)
+        return len(prev_kills.loc[prev_kills['Time'] > (minute-time_range)].index)/time_range
+
+    
+#-----------------------------------REDUNDANT FUNCTION-----------------------------------#    
+    def last_kill(self,minute,team_type = "all"):
+        '''Returns the time since last kill, the kill object, and its index.'''
+        
+        prev_kills = self.prev_kills(minute,team_type)
+        
+        if team_type == "red":
+            kill_list = self.red_kills()
+        elif team_type == "blue":
+            kill_list = self.blue_kills()
+        else:
+            kill_list = self.kills()
+        
+        if prev_kills != {}:
+            last_kill_time = max(prev_kills)
+            delta_time = minute - last_kill_time
+            index = prev_kills[last_kill_time]
+            return delta_time,kill_list.values[index],index
+        else:
+            return None
+#----------------------------------------------------------------------------------------#   
+
+
+
     def get_xy(self,index):
-        #Returns the X,Y coordinates of a requested kill
+        '''Returns the X,Y coordinates of a requested kill'''
+        
         kill = self.kills().values[index]
         return kill[9],kill[10]
 
     def nexus_distance(self,xy):
-        #returns the distance of a set of x,y coordinates from the blue team's nexus.
+        '''returns the distance of a set of x,y coordinates from the blue team's nexus.'''
+        
 #         return math.dist(xy,(0,0))
-         return math.sqrt(int(xy[0])**2+int(xy[1])**2)
+        return math.sqrt(int(xy[0])**2+int(xy[1])**2)
     
     # ===== MONSTERS =====
     def monsters(self):
@@ -90,29 +142,105 @@ class match:
         self_structures = self.structures()
         return self_structures.loc[(self_structures['Team'] == 'rTowers') | (self_structures['Team'] == 'rInhibs')]
     
-    def last_structure(self,minute):
-        #Returns the time since last structure taken, the structure object, and its index.
-        prev_structures = {}
-        self_structures = self.structures()
+    def prev_structures(self,minute,team_type = "all"):
+        '''Returns a dictionary object with all structures taken before a specified minute, containing the time of event, and its index.
+        Also takes a team type argument for specifying, blue, red, or all.'''
+
+        prev_structures = pd.DataFrame({'Time' : [],'Index':[],'Blue':[]})
+        
+        if team_type == "red":
+            self_structures = self.red_structures()
+        elif team_type == "blue":
+            self_structures = self.blue_structures()
+        else:
+            self_structures = self.structures()
+        
         for index,structure in enumerate(self_structures.values):
             a = (minute - structure[2])
             if a > 0:
-                prev_structures[str(a)] = index
-        return min(prev_structures),self_structures.values[prev_structures[min(prev_structures)]],prev_structures[min(prev_structures)]
+                this_structure = pd.DataFrame({'Time' : [structure[2]],
+                                               'Index':[index],
+                                               'Blue':[(structure[1] == 'bTowers')|(structure[1] == 'bInhibs')]})
+                
+                prev_structures = pd.concat([prev_structures,this_structure],ignore_index=True)
+                
+        return prev_structures.sort_values('Time').reset_index(drop=True)
     
+    def structure_freq(self,minute,time_range = 5,team_type = "all"):
+        '''Gets the kill frequency from a given minute, over a given range (default = 5 mins),
+        for either blue, red, or all teams. (default = all)'''
+        prev_structures = self.prev_structures(minute,team_type)
+        return len(prev_structures.loc[prev_structures['Time'] > (minute-time_range)].index)/time_range
+
+    
+#-----------------------------------REDUNDANT FUNCTION-----------------------------------#
+    def last_structure(self,minute,team_type = "all"):
+        '''Returns the time since last structure taken, the structure object, and its index.'''
+        prev_structures = self.prev_structures(minute,team_type)
+        
+        if team_type == "red":
+            structure_list = self.red_structures()
+        elif team_type == "blue":
+            structure_list = self.blue_structures()
+        else:
+            structure_list = self.structures()
+            
+        if prev_structures != {}:
+            last_structure_time = max(prev_structures)
+            delta_time = minute - last_structure_time
+            index = prev_structures[last_structure_time]
+            return delta_time,structure_list.values[index],index
+        else:
+            return None
+#----------------------------------------------------------------------------------------#       
+
+
+
     # ===== GAME STATE TEST =====
     def game_state(self,minute):
-        #Returns a test gamestate tuple for a given minute.
+        '''Returns a test gamestate tuple for a given minute.'''
+        nd = None
         minute = minute
-        last_kill = self.last_kill(minute)
-        last_structure = self.last_structure(minute)
+        prev_kills = self.prev_kills
+        prev_structures = self.prev_structures
+#         last_kill = self.last_kill(minute)
+#         if last_kill != nd:
+#             last_kill_distance = [self.nexus_distance(self.get_xy(last_kill[2]))]
+#             last_kill_blue = [last_kill[1][1] == "bKills"]
+#             last_kill_time = [last_kill[0]]
+#         else:
+#             last_kill_blue = [nd]
+#             last_kill_distance = [nd]
+#             last_kill_time = [nd]
+            
+#         last_structure = self.last_structure(minute)
+#         if last_structure != nd:
+#             last_structure_blue = [last_structure[1][1] == "bTowers"]
+#             last_structure_time = [last_structure[0]]
+#             prev_red_structures = [len(self.prev_structures(minute,"red").index)]
+#             prev_blue_structures = [len(self.prev_structures(minute,"blue").index)]
+#         else:
+#             last_structure_blue = [nd]
+#             last_structure_time = [nd]
+#             prev_red_structures = [nd]
+#             prev_blue_structures = [nd]
+        
+        golddiff = self.gold_type("golddiff")[minute-1]
+        oldgolddiff = self.gold_type("golddiff")[minute-4] if minute>3 else 0
         data = {"minute":minute,
-                "Time since last kill":[last_kill[0]], 
-                "Last kill blue":[last_kill[1][1] == "bKills"],
-                "last kill distance":[self.nexus_distance(self.get_xy(last_kill[2]))],
-                "Time since last structure":[last_structure[0]],
-                "Last structure blue":[last_structure[1][1] == "bTowers"],
-                "Gold diff":[self.gold_type("golddiff")[minute-1]]
+                "Blue kills":[len(prev_kills(minute,"blue").index)],
+                "Blue Deaths":[len(prev_kills(minute,"red").index)],
+                "Kill freq":[self.kill_freq(minute)],
+                "Blue kill freq":[self.kill_freq(minute,team_type = "blue")],
+                "Blue death freq":[self.kill_freq(minute,team_type = "red")],
+                "Red structures taken":[len(prev_structures(minute,"blue").index)],
+                "Blue structures lost":[len(prev_structures(minute,"red").index)],
+                "Structure freq":[self.structure_freq(minute)],
+                "Blue structure freq":[self.structure_freq(minute,team_type = "blue")],
+                "Red structure freq":[self.structure_freq(minute,team_type = "red")],
+                "Gold diff":[golddiff],
+                "Delta Golld diff":[golddiff - oldgolddiff],
+                "Blue Win": [self.bResult()]
                }
         return pd.DataFrame.from_dict(data)
     
